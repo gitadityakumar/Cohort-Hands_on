ddf0e18279ff347a1a80e6e2eaedfc335b7e19b6
gitadityakumar <itsadityasingh05@gmail.com>
2024-02-26 23:45:56 +0530

    input validation still not using zod for input valdiation

--- Week-3/week-3.1/index.js
+++ Week-3/week-3.1/index.js
@@ -2,23 +2,27 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/api/v1/health-checkup', (req, res) => {
-    const KidneyId = req.query.KidneyId;
-    const username = req.headers.username;
-    const password = req.headers.password;
+app.use(express.json());
 
-    if(username != "Aditya" || password != "pass"){
-        res.status(400).json({"msg":"someting worng with inputs"})
-        return
-    }
+//input validation
+app.post('/api/v1/health-checkup', (req, res) => {
+    const kidneys = req.body.kidneys;
+    const kidneyLength = kidneys.length;
 
-    if(KidneyId !=1 && KidneyId != 2){
-        res.status(400).json({"msg":"someting worng with inputs"})
-        return
-    }
-    res.json({
-        "msg": "your kidney is fine"
-    })
+    res.send("you have "+ kidneyLength+ "kidneys");
+
+    // if(username != "Aditya" || password != "pass"){
+    //     res.status(400).json({"msg":"someting worng with inputs"})
+    //     return
+    // }
+
+    // if(KidneyId !=1 && KidneyId != 2){
+    //     res.status(400).json({"msg":"someting worng with inputs"})
+    //     return
+    // }
+    // res.json({
+    //     "msg": "your kidney is fine"
+    // })
 
   
     // res.send("your heart is Healthy");
