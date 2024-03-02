55ce49da3254bab53cec2b9346305df06b080817
gitadityakumar <itsadityasingh05@gmail.com>
2024-03-02 23:25:47 +0530

    cohort1-100 Nextjs intiation

---
+++ cohort1-100/week-13.1/nextapp/app/Sign/layout.tsx
@@ -0,0 +1,12 @@
+export default function RootLayout({
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) {
+    return (
+        <div className="p-4 border-b">
+            20% off in new courses 
+        </div>
+     
+    );
+  }
