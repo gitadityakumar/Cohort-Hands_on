38f12254291206c5ee518841a8b5bb2566b83088
gitadityakumar <itsadityasingh05@gmail.com>
2024-03-09 00:58:54 +0530

    removed sm file

--- cohort1-100/week14.2/nextserverside/app/api/user/route.ts
+++ cohort1-100/week14.2/nextserverside/app/api/user/route.ts
@@ -1,7 +1,15 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+
 export async function GET(){
     return Response.json({
         name:"Aditya kumar",
         email:"aditya@gmail.com"
     })
+}
 
+export async function POST(req: NextRequest) {
+    const body = await req.json();
+
+    return NextResponse.json({ username: body.username, password: body.password })
 }
