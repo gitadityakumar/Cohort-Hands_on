6d47b485dfc7b4d855a274a241826e2fddf53003
gitadityakumar <itsadityasingh05@gmail.com>
2024-03-17 20:47:50 +0530

    week 16.1.2

---
+++ cohort1-100/week-16/week16.1.1/packages/common/src/index.ts
@@ -0,0 +1,3 @@
+export const VALUE = "Aditya kumar";
+
+export const BACKEND_URL = "https://api.google.com";
---
+++ cohort1-100/week-16/week16.1.1/packages/ui/src/admin.tsx
@@ -0,0 +1,10 @@
+
+"use client"
+
+export function Admin(){
+    return(
+        <div>
+            Admin page
+        </div>
+    );
+}
