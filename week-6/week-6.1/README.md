a4a6a31e76469615136f4b3fae0b65ef9087ccda
gitadityakumar <itsadityasingh05@gmail.com>
2024-04-10 05:13:32 +0530

    done rendering in react

---
+++ week-6/week-6.1/README.md
@@ -0,0 +1,4 @@
+# Learnings
+
+1. minimize re-renders in react 
+2. How to minimize re-renders :
---
+++ week-6/week-6.1/src/App.jsx
@@ -0,0 +1,32 @@
+
+import { useState } from "react";
+
+function App() {
+ const [title, setTitle] = useState("my name is Aditya kumar");
+
+ function updateTitle(){
+  setTitle("my name is "+ Math.random());
+ }
+ 
+
+  return (
+    <div>
+    <button onClick={updateTitle}>Update the title</button>
+     <Header title={title}/>
+     <Header title="Header1"/>
+     <Header title="Header2"/>
+     <Header title="Header3"/>
+
+
+     
+    </div>
+  )
+}
+
+function Header({title}){
+  return <div>
+    {title}
+  </div>
+}
+
+export default App
