d754583acbbb5c5d3bf70ca82887dc055e889dfc
gitadityakumar <itsadityasingh05@gmail.com>
2024-04-10 05:32:23 +0530

    component with states

--- week-6/week-6.1/src/App.jsx
+++ week-6/week-6.1/src/App.jsx
@@ -2,31 +2,38 @@
 import { useState } from "react";
 
 function App() {
- const [title, setTitle] = useState("my name is Aditya kumar");
 
- function updateTitle(){
-  setTitle("my name is "+ Math.random());
- }
- 
+  return (
+    <div>
+      <HeaderWtihButtonn/>
+     <Header title="Header1"/>
+     <Header title="Header2"/>
+    </div>
+  )
+}
+//component that causes unncessary re-renders
+function Header({title}){
+  return <div>
+    {title}
+  </div>
+}
+
+//comonent that doesn't cause extra re-renders
+function HeaderWtihButtonn(){
+  const [title, setTitle] = useState("my name is Aditya kumar");
+
+  function updateTitle(){
+   setTitle("my name is "+ Math.random());
+  }
 
   return (
     <div>
     <button onClick={updateTitle}>Update the title</button>
      <Header title={title}/>
-     <Header title="Header1"/>
-     <Header title="Header2"/>
-     <Header title="Header3"/>
-
-
-     
+     <Header title="Header with button"/>
+    
     </div>
   )
-}
-
-function Header({title}){
-  return <div>
-    {title}
-  </div>
 }
 
 export default App
