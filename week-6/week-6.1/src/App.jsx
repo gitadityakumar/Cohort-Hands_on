cf84aa6892aa55fd8b347ec40f1d4f0c61501867
gitadityakumar <itsadityasingh05@gmail.com>
2024-04-10 05:44:59 +0530

    done React-memo

--- week-6/week-6.1/src/App.jsx
+++ week-6/week-6.1/src/App.jsx
@@ -1,39 +1,39 @@
 
 import { useState } from "react";
+import React from "react";
 
 function App() {
+ const [title, setTitle] = useState("my name is Aditya kumar");
 
-  return (
-    <div>
-      <HeaderWtihButtonn/>
-     <Header title="Header1"/>
-     <Header title="Header2"/>
-    </div>
-  )
-}
-//component that causes unncessary re-renders
-function Header({title}){
-  return <div>
-    {title}
-  </div>
-}
-
-//comonent that doesn't cause extra re-renders
-function HeaderWtihButtonn(){
-  const [title, setTitle] = useState("my name is Aditya kumar");
-
-  function updateTitle(){
-   setTitle("my name is "+ Math.random());
-  }
+ function updateTitle(){
+  setTitle("my name is "+ Math.random());
+ }
+ 
 
   return (
     <div>
     <button onClick={updateTitle}>Update the title</button>
      <Header title={title}/>
-     <Header title="Header with button"/>
-    
+     <Header title="Header1"/>
+     <Header title="Header2"/>
+     <Header title="Header3"/>
+
     </div>
   )
 }
+
+// function Header({title}){
+//   return <div>
+//     {title}
+//   </div>
+// }
+
+
+
+const Header = React.memo(function Header({title}){
+  return <div>
+    {title}
+  </div>
+});
 
 export default App
