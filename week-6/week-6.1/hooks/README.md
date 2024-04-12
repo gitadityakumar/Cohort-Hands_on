b5274e3a2c92d7c70407d614722bb1e2210f41e9
gitadityakumar <itsadityasingh05@gmail.com>
2024-04-12 21:27:39 +0530

    done useEffect but pooling is left

---
+++ week-6/week-6.1/hooks/README.md
@@ -0,0 +1,7 @@
+# Hooks in React
+
+## Jargons : lifecycle event , Mount , 
+
+1. In class based comonents where we get access to lifecycle functions , which was onComponentMount() and other bunch of like these.
+
+2. 
---
+++ week-6/week-6.1/hooks/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+
+    fetch("https://sum-server.100xdevs.com/todos")
+    .then(async function(res){
+      const json = await res.json();
+      setTodos(json.todos);
+    })
+    
+  }, [])
+  
+  
+
+  
+  return (
+    <>
+     <h1 style={{ textAlign: 'center', backgroundColor: '#4CAF50', color: '#ffffff', padding: '20px', borderRadius: '5px', boxShadow: '0 2px 5px rgba(0,0,0,0.1)' }}>Todo from server using useEffect</h1>
+
+     <div>
+      {todos.map(todo => <Todo key={todo.id} title={todo.title} description={todo.description} />)}
+     </div>
+       
+    </>
+  )
+}
+
+function Todo({ title, description }) {
+  return (
+    <>
+      <div style={{ padding: '20px', backgroundColor: '#f0f0f0', borderRadius: '5px', boxShadow: '0 2px 5px rgba(0,0,0,0.1)' }}>
+       <h1> {title}</h1>
+        <h2> {description}</h2>
+      </div>
+    </>
+  );
+}
+
+
+export default App
