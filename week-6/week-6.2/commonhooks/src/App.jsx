2a80b13e62b30398936aff1cccfa6dfd4d6e0137
gitadityakumar <itsadityasingh05@gmail.com>
2024-04-14 11:01:16 +0530

    added button to change todos

--- week-6/week-6.2/commonhooks/src/App.jsx
+++ week-6/week-6.2/commonhooks/src/App.jsx
@@ -2,31 +2,41 @@ import { useState } from "react";
 import { useEffect } from "react";
 
 function App() {
-  return <div>
-    <Todo id={1} />
-  </div>
+  const [idupdate, setIdupdate] = useState(1);
+
+  function idUpdater(id) {
+    setIdupdate(id);
+  }
+
+  return (
+    <div>
+      <button onClick={() => idUpdater(1)}>1</button>
+      <button onClick={() => idUpdater(2)}>2</button>
+      <button onClick={() => idUpdater(3)}>3</button>
+      <button onClick={() => idUpdater(4)}>4</button>
+      <Todo id={idupdate} />
+    </div>
+  );
 }
 
 // eslint-disable-next-line react/prop-types
-function Todo({id}) {
+function Todo({ id }) {
   const [todo, setTodo] = useState({});
 
   useEffect(() => {
     fetch("https://sum-server.100xdevs.com/todo?id=" + id)
-      .then(async function(res) {
+      .then(async function (res) {
         const json = await res.json();
-        setTodo (json.todo);
-      })
-  }, [id])
+        setTodo(json.todo);
+      });
+  }, [id]);
 
-  return <div>
-    <h1>
-      {todo.title}
-    </h1>
-    <h4>
-      {todo.description}
-    </h4>
-  </div>
+  return (
+    <div>
+      <h1>{todo.title}</h1>
+      <h4>{todo.description}</h4>
+    </div>
+  );
 }
 
 export default App;
