819c6873dcd8add144c667c491004cfe7d87c59d
gitadityakumar <itsadityasingh05@gmail.com>
2024-04-14 10:30:56 +0530

    done useEffect

---
+++ week-6/week-6.2/commonhooks/README.md
@@ -0,0 +1,6 @@
+## common Hooks 
+
+what is sideEffect in react ?
+
+1. useEffect => useEffect(setup, dependencies?) 
+   dependencies= state variables
---
+++ week-6/week-6.2/commonhooks/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://sum-server.100xdevs.com/todos')
+    .then(function(response){
+      setTodos(response.data.todos)
+    })
+    
+  }, [])
+  
+
+  return (
+    <>
+      {todos.map(todo => <Todo title={todo.title} description={todo.description} key={todo.id} />)}
+    </>
+  )
+
+  function Todo({ title, description }) {
+    return (
+        <div>
+          <h1>{title}</h1>
+          {description}
+        </div>
+    )
+  }
+}
+
+export default App
