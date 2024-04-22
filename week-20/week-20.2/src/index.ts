c7632db417295cec4b14496a7ac0d555e188848d
gitadityakumar <itsadityasingh05@gmail.com>
2024-04-22 22:57:15 +0530

    tried pubsub with singleton approach

--- week-20/week-20.2/src/index.ts
+++ week-20/week-20.2/src/index.ts
@@ -1,15 +1,5 @@
-import { gameManager } from "./store";
-import { startLogger } from "./logger";
-
-
-
-startLogger();
+import { PubSubManager } from "./PubSubManager";
 
 setInterval(() => {
-    gameManager.addGame({
-        id: Math.random().toString(),
-        "whitePlayer": "harkirat",
-        "blackPlayer": "jaskirat",
-        moves: []
-    })
+    PubSubManager.getInstance().userSubscribe(Math.random().toString(), "APPL");
 }, 5000)
--- week-20/week-20.2/src/logger.ts
+++
@@ -1,9 +0,0 @@
-import { GameManager } from "./store";
-
-const gameManager = new GameManager();
-
-export function startLogger() {
-    setInterval(() => {
-        gameManager.logState();
-    }, 4000)
-}
