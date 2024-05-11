aa8689896a8bd0e86e5e2b60191c506fda53023d
gitadityakumar <itsadityasingh05@gmail.com>
2024-05-11 20:54:14 +0530

    Restr

---
+++ week-23/Takeaways.md
@@ -0,0 +1,4 @@
+# Remote procedure Call (rpc)
+
+1. when we're dealing with millions of user or we are at a scale then these method/tecnique is used 
+
--- week-6/week-6.3/Recap/src/App.jsx
+++ week-6/week-6.3/Recap/src/App.jsx
@@ -1,42 +1,19 @@
-import { useEffect, useMemo, useState } from 'react'
+import {  useEffect, useRef  } from 'react'
 
 function App() {
-  const [exchange1Data, setExchange1Data] = useState({});
-  const [exchange2Data, setExchange2Data] = useState({});
-  const [bankData, setBankData] = useState({});
+  const divRef = useRef();
 
   useEffect(() => {
-    // Some operation to get the data
-    setExchange1Data({
-      returns: 100
-    });
-  }, [])
-
-  useEffect(() => {
-    // Some operation to get the data
-    setExchange2Data({
-      returns: 100
-    });
-  }, [])
-
-  useEffect(() => {
-    // Some operation to get the data
     setTimeout(() => {
-      setBankData({
-        income: 100
-      });
-    },5000)
+      divRef.current.innerHTML = "10"
+    }, 5000);
   }, [])
 
-  const cryptoReturns = useMemo(()=>{
-   return exchange1Data.returns + exchange2Data.returns;
-  },[exchange1Data,exchange2Data ]);
-  
-  const incomeTax = (cryptoReturns + bankData.income) * 0.3
+  const incomeTax = 20000;
 
   return (
     <div>
-        hi there, your income tax returns are {incomeTax}
+        hi there, your income tax returns are <div ref={divRef}>{incomeTax}</div>
     </div>
   )
 }
