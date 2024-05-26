f55222f8250e7e78994d911746af28b2be549f65
gitadityakumar <itsadityasingh05@gmail.com>
2024-05-26 20:23:58 +0530

    done requestCount

---
+++ week-25/week-25.2.live/prom/src/index.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import { middleware } from "./middleware";
+import client from "prom-client";
+import { requestCountMiddleware } from "./metrics/requestCount";
+
+const app = express();
+app.use(middleware);
+app.use(requestCountMiddleware);
+
+app.use(express.json());
+
+app.get("/user", (req, res) => {
+    res.send({
+        name: "John Doe",
+        age: 25,
+    });
+});
+
+
+
+app.get("/metrics", async (req, res) => {
+    const metrics = await client.register.metrics();
+    res.set('Content-Type', client.register.contentType);
+    res.end(metrics);
+})
+
+app.post("/user", (req, res) => {
+    const user = req.body;
+    res.send({
+        ...user,
+        id: 1,
+    });
+});
+
+app.listen(3000);
