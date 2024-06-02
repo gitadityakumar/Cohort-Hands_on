045074169750bf0d6fe98467accbc9127f7b09ac
gitadityakumar <itsadityasingh05@gmail.com>
2024-06-02 13:58:32 +0530

    done Lazyloading

--- week-7/Routing/src/App.jsx
+++ week-7/Routing/src/App.jsx
@@ -1,23 +1,31 @@
-import { Route,BrowserRouter,Routes,  } from 'react-router-dom'
-import Dashboard from './components/Dashboard'
-import Landing from './components/Landing'
-import Register from './components/Register'
-import Appbar from './components/Appbar'
+import   { lazy, Suspense } from 'react';
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import { ClipLoader } from 'react-spinners';  // Import a specific spinner from react-spinners
+import Appbar from './components/Appbar';
+
+const Landing = lazy(() => import('./components/Landing'));
+const Register = lazy(() => import('./components/Register'));
+const Dashboard = lazy(() => import('./components/Dashboard'));
 
 function App() {
- 
   return (
     <div>
       <BrowserRouter>
-      <Appbar />
-        <Routes>
-          <Route path="/" element={<Landing/>}/>
-          <Route path="/register" element={<Register/>}/>
-          <Route path="/dashboard" element={<Dashboard/>}/>
-        </Routes>
+        <Appbar />
+        <Suspense fallback={
+          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+            <ClipLoader size={50} color={"#123abc"} loading={true} />
+          </div>
+        }>
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Routes>
+        </Suspense>
       </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
