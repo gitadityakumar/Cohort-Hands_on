2b1f5a76e8b51a6d23e52600d4423ad887e07139
gitadityakumar <itsadityasingh05@gmail.com>
2024-06-03 19:00:05 +0530

    contextapi

--- week-7/propdrilling/src/App.jsx
+++ week-7/propdrilling/src/App.jsx
@@ -1,26 +1,38 @@
-import { useState } from 'react'
+import { useContext, useState } from 'react'
+import { CountContext } from './context';
 
 
 function App() {
-   const [count, setCount] = useState(0)
+   const [count, setCount] = useState(0);
 
   return (
     <>
+     <CountContext.Provider value={count}>
      <Count count={count} setCount={setCount}/>
+     </CountContext.Provider>
+     
     </>
   )
 }
 
+// eslint-disable-next-line react/prop-types
 function  Count({count,setCount}){
   return <div>
-    <h1>{count}</h1>
+    <CountRenderer count={count} />
     <Buttons count={count} setCount={setCount}/>
     {/* this is prodrilling */}
 
   </div>
 }
 
-function Buttons({count,setCount}){
+// eslint-disable-next-line react/prop-types
+function CountRenderer(){
+  const count = useContext(CountContext);
+  return <h1>{count}</h1>
+}
+// eslint-disable-next-line react/prop-types
+function Buttons({setCount}){
+  const count = useContext(CountContext);
     return <>
     <button onClick={()=>{setCount(count+1)}} style={{backgroundColor:'green'}}>Increase</button>
     <button onClick={()=>{setCount(count-1)} } style={{backgroundColor:'red'}}>Decrease</button>
