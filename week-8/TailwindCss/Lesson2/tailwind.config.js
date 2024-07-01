1b9e94813a8c924c64c60f92d40ac4bd73c072df
gitadityakumar <itsadityasingh05@gmail.com>
2024-07-01 18:37:57 +0530

    mobile-menu & hamburger-icon done

--- week-8/TailwindCss/Lesson2/tailwind.config.js
+++ week-8/TailwindCss/Lesson2/tailwind.config.js
@@ -1,11 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./build/*.html"],
+  content: ["./build/*.html","./build/js/*.js"],
   theme: {
     extend: {
       screen:{
         'widescreen':{'rew': '(min-aspect-ratio:3/2)'},
         'tallscreen':{'raw': '(min-aspect-ratio:1/2)'},
+      },
+      keyframs:{
+        'open-menu':{
+          '0%':{transform:'scaleY(0)'},
+          '80%':{transform:'scaleY(1.2)'},
+          '100%':{transform:'scaleY(1)'},
+        },
+      },
+      animaiton:{
+        'open-menu':'open-menu 0.5s ease-in-out forwards',
       }
     },
   },
